7e69b6406c18608f2eb1e967ab3eddc9148d862f
homean <hohohosky101@gmail.com>
2023-07-19 11:04:32 +0900

    리액트에서 빌드된 파일 노드 서버를 통해서 구동

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+const path = require('path');
+
+//express에서 client측 build폴더까지의 경로를 static하게 지정
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.get('/', (req, res) => {
-	res.send('Hello World3');
+	res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-app.listen(port, () => {
-	console.log(`app is listening in port ${port}`);
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
